92833bfdfd8e6d8c3f578c9efa449339c7d25c09
@AstroAura <ismailoozeerally28@gmail.com>
2026-01-30 21:09:57 +0400

    Added wallpaper rotator

--- fish/.config/config.fish
+++ fish/.config/config.fish
@@ -24,4 +24,10 @@ zoxide init fish | source
 # Set YT_X_FZF_OPTS globally
 set -Ux YT_X_FZF_OPTS "--color=fg:#e0def4,fg+:#e0def4,bg:#232136,bg+:#44415a --color=hl:#3e8fb0,hl+:#9ccfd8,info:#f6c177,marker:#3e8fb0 --color=prompt:#eb6f92,spinner:#c4a7e7,pointer:#c4a7e7,header:#c4afcb --color=border:#44415a,label:#ea9a97,query:#f6c177 --border=rounded --border-label='' --preview-window=border-rounded --prompt='> ' --marker='>' --pointer='◆' --separator='─' --scrollbar='│'"
 
-
+function lsi
+    set -l output (ls-interactive "$argv")
+    if test $status -eq 0
+        and test -n "$output"
+        cd $output
+    end
+end
